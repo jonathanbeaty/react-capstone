55cde257da73d5e61e92ded561ca75f68b3d1545
Jonathan Beaty <jonathanbeaty15@gmail.com>
2019-02-26 05:42:21 -0600

    react-create-app on Heroku

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-exports.API_BASE_URL = process.env.REACT_APP_API_ORIGIN || 'https://react-capstone-node.herokuapp.com/' || 'http://localhost:8080/'
+exports.API_BASE_URL = process.env.REACT_APP_API_ORIGIN || 'http://localhost:8080/'
