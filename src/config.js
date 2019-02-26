7d96337a1d8c639a79115b64a3cdeb0dc06186d8
Jonathan Beaty <jonathanbeaty15@gmail.com>
2019-02-25 21:08:19 -0600

    updating heroku

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-exports.API_BASE_URL = process.env.REACT_APP_API_ORIGIN || 'https://dashboard.heroku.com/apps/react-capstone-node' || 'http://localhost:8080/'
+exports.API_BASE_URL = process.env.REACT_APP_API_ORIGIN || 'https://react-capstone-node.herokuapp.com/' || 'http://localhost:8080/'
